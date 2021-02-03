33572f5ca872b48187aa6db4238e175a3c4dcfef
mentari <mentari.er@gmail.com>
2021-02-03 19:47:00 +0700

    Edit Nama, Password, dan Email

---
+++ app/src/main/java/proyek/android/iqra/EditEmailActivity.java
@@ -0,0 +1,14 @@
+package proyek.android.iqra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_email);
+    }
+}
